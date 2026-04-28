bccfe4670264bf84b17c2165aaa52e5091790593
ayaahmedelrayes <ayaahmedelrayes@gmail.com>
2026-04-28 12:03:47 +0300

    Refactor server.js to handle port configuration
    Move port definition inside the NODE_ENV check for better environment handling.

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const app =require('./app');
 const connectDB=require('./config/db');
 const dotenv=require('dotenv');
 dotenv.config();
-const port =process.env.PORT || 5000;
 connectDB();
-app.listen(port,()=>{
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 5000;
+  app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+  });
+}
+module.export=app;
