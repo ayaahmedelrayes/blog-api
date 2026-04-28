a130941c0df4c36ce75d9556b61a3b96888527d8
ayaahmedelrayes <ayaahmedelrayes@gmail.com>
2026-04-28 11:59:12 +0300

    Implement CORS middleware in app.js
    Added CORS middleware to handle cross-origin requests and enabled OPTIONS method support.

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const mongoose = require('mongoose');
 const errorHandler = require('./middleware/error.middleware');
 const userRoutes = require('./routes/user.routes');
 const app=express();
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
-// app.get('/',(req,res)=>{
-//     res.json({message:'Welcome to the blog API'})
-// })
+ app.get('/',(req,res)=>{
+    res.json({message:'Welcome to the blog API'})
+})
 const authRoutes=require('./routes/auth.routes');
 const postRoutes=require('./routes/post.routes');
 const groupRoutes=require('./routes/group.routes');
